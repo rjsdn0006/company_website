36128a1176df2344b42dd82c295af435a7027d28
rjsdn0006 <rjsdn0006@gmail.com>
2025-02-24 16:52:40 +0900

    Forum

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,60 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import About from "./Pages/About/About";
+import MainPage from "./Pages/MainPage/MainPage";
+import Leadership from "./Pages/Leadership/Leadership";
+import Board from "./Pages/Board/Board";
+import Services from "./Pages/Services/Services";
+import Contact from "./Pages/Contact/Contact";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // 자식요소들이 Outlet에 보여지게 된다.
+      {
+        index: true, // 기본경로 일경우
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ frontend/src/Pages/Leadership/Leadership.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Leadership = () => {
+  return <div>Leadership</div>;
+};
+
+export default Leadership;
